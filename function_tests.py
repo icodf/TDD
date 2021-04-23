b628c4f26b273e2a50e500f10d3eeb7ebc514cf8
unknown <3326268527@qq.com>
2021-04-23 17:53:22 +0800

    Model for list Items and associated migration

--- function_tests.py
+++ function_tests.py
@@ -34,7 +34,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -42,7 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         # The page updates again, and now shows both item on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')

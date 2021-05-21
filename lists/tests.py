1794bff0a5d87371077e474f186edd2892280013
unknown <3326268527@qq.com>
2021-05-21 14:24:19 +0800

    Add a URL for creating a new list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -50,8 +50,9 @@ class ListViewTest(TestCase):
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post('/list/new', data={'item_text':'A new list item'})
-        self.assertEqual(Item.objexts.count(),1)
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 

7c48efcb0c2554981ad9e66db31ca05e0288d3eb
unknown <3326268527@qq.com>
2021-05-22 15:59:35 +0800

    3 changed files, plus 2 migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from lists.models import Item
 from lists.views import home_page
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -40,8 +40,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_diaplays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -61,36 +62,31 @@ class NewListTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
+
+class ListAndItemModelTest(TestCase):
+    def test_saving_And_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
-        first_item.text = 'The fitst (ever) list item'
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
-        second_item.save();
+        second_item.list = list_
+        second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
+        self.assertEqual(saved_items.count(),2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The fitst (ever) list item')
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list,list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
-
-#    def test_root_url_resolve_to_home_page_view(self):
-#        found = resolve('/')
-#        self.assertEqual(found.func, home_page)
-#
-#    def test_home_page_returns_correct_html(self):
-#        response = self.client.get('/')
-#
-#        html = response.content.decode('utf8')
-#        self.assertTrue(html.startswith('<html>'))
-#        self.assertIn('<title>To-Do lists</title>', html)
-#        self.assertTrue(html.strip().endswith('</html>'))
-#
-#        self.assertTemplateUsed(response, 'home.html')
-
-# Create your tests here.
+        self.assertEqual(second_saved_item.list,list_)

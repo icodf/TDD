6dd970edfc3035ea9eeda9db2a6e71bd46c55c6d
unknown <3326268527@qq.com>
2021-05-28 15:28:57 +0800

    new URL + view for adding to existing lists. FT passes

--- lists/models.py
+++ lists/models.py
@@ -5,7 +5,7 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
 
 # Create your models here.
